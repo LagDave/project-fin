f79efcac6c7e9d94baa5100300792b4e5d2b2b84
lagdave <laggy80@gmail.com>
2023-04-25 22:28:59 +0800

    added action buttons

--- src/components/Dashboard/StashItem.tsx
+++ src/components/Dashboard/StashItem.tsx
@@ -1,16 +1,30 @@
-import { Card, Grid, Text } from "@mantine/core";
+import { IconPencil, IconTrash } from "@tabler/icons-react";
+import "./stash-item.styles.scss";
+
+import { Button, Card, Grid, Text, Flex } from "@mantine/core";
 
 export default function StashItem({details}: any) {
   return (
-    <Card>
-      <Grid>
-        <Grid.Col span={8}>
-          <Text fz="sm" fw="bold">{details.name}</Text>
-        </Grid.Col>
-        <Grid.Col span={4} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Text fz="xs">{details.total}</Text>
-        </Grid.Col>
-      </Grid>
-    </Card>
+    <div className="stash-item">
+      <Card>
+        <Grid>
+          <Grid.Col style={{ display: 'flex', alignItems: 'center' }} span={8}>
+            <Text fz="sm" fw="bold">{details.name}</Text>
+          </Grid.Col>
+          <Grid.Col span={4} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Flex align="center" gap='xs'>
+                {/* <Text fz="xs">{details.total}</Text> */}
+                <Text fz="xs">990,232.23</Text>
+            </Flex>
+          </Grid.Col>
+        </Grid>
+      </Card>
+
+      <Flex className="stash-action-buttons" gap="5px">
+        <Button className="delete-button" color="red.3" compact><IconTrash size='1rem'/></Button>
+        <Button className="delete-button" color="dark.2" compact><IconPencil size='1rem'/></Button>
+      </Flex>
+      
+    </div>
   )
 }
